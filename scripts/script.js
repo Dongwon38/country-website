3b98e5abad29e1aa2623bfe207ce38fd42c99cc1
Dongwon Kang <dongwon.dwk@gmail.com>
2024-05-27 23:50:29 -0700

    deleted non-used codes on script

--- scripts/script.js
+++ scripts/script.js
@@ -2,17 +2,6 @@
 const toggleBtn = document.getElementById("toggle-button");
 const togglePg = document.getElementById("toggle-page");
 
-// // for toggle accessibility
-// toggleBtn.addEventListener("change", () => {
-//   toggleBtn.setAttribute("aria-checked", toggleBtn.checked);
-// });
-
-// // page toggle
-// toggleBtn.addEventListener("click", () => {
-//   togglePg.classList.toggle("toggled");
-//   console.log("clickc");
-// });
-
 // nav list
 
 const navBtn = document.getElementById("nav-btn");
